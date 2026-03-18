4260b00d7770b97885e8be7ee7aa32c43ac6bd45
EnricoAncaraniUnibo <enrico.ancarani@studio.unibo.it>
2026-03-18 19:57:34 +0100

    asf

--- ESP32/ESP32.ino
+++ ESP32/ESP32.ino
@@ -7,13 +7,22 @@ CheckingWaterLevelTask* CheckingWater;
 Platform* p;
 
 void setup() {
+  Serial.begin(115200);
   p = new Platform();
   p->getRedLed()->switchOn();
   p->getGreenLed()->switchOff();
   CheckingWater = new CheckingWaterLevelTask(p->getSonar());
-  xTaskCreatePinnedToCore(CheckingWater->tick(),"CheckingWaterLevelTask",10000,NULL,1,&CheckingWaterTask,0);
+  xTaskCreatePinnedToCore(CheckingWater,"CheckingWaterLevelTask",10000,NULL,1,&CheckingWaterTask,0);
 }
 
 void loop() {
 
+}
+
+void waterTaskWrapper(void* param) {
+  while(true) {
+    CheckingWater->tick();
+    Serial.println(CheckingWater->getLastDistance());
+    vTaskDelay(1000);
+  }
 }
