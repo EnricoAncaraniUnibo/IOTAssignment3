a1c2789c79da7ec08a3da5f9de3b360f64301c97
EnricoAncaraniUnibo <enrico.ancarani@studio.unibo.it>
2026-03-18 19:46:40 +0100

    sfa

--- ESP32/ESP32.ino
+++ ESP32/ESP32.ino
@@ -1,22 +1,14 @@
-#include "src/kernel/SchedulerESP.h"
 #include "src/tasks/CheckingWaterLevelTask.h"
 #include "src/model/Platform.h"
 #include "src/model/ESP32.h"
-Scheduler sched;
-CheckingWaterLevelTask* checkingWaterTask;
-Platform* p;
-Esp32* esp;
+
+
 
 void setup() {
-  sched.init(50);
-  p=new Platform();
-  esp = new Esp32();
-  checkingWaterTask = new CheckingWaterLevelTask(p->getSonar());
-  sched.addTask(checkingWaterTask);
-  p->getRedLed()->switchOn();
+  Serial.begin(115200);
+  Serial.println(xPortGetCoreID());
 }
 
 void loop() {
-  sched.schedule();
 
 }
