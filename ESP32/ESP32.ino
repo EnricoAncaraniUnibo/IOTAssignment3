4424eee05b52fe141abdc2c12014b78a85400f2a
EnricoAncaraniUnibo <enrico.ancarani@studio.unibo.it>
2026-03-18 19:51:29 +0100

    asf

--- ESP32/ESP32.ino
+++ ESP32/ESP32.ino
@@ -2,11 +2,16 @@
 #include "src/model/Platform.h"
 #include "src/model/ESP32.h"
 
-
+TaskHandle_t CheckingWaterTask;
+CheckingWaterLevelTask* CheckingWater;
+Platform* p;
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println(xPortGetCoreID());
+  p = new Platform();
+  p->getRedLed()->switchOn();
+  p->getGreenLed()->switchOff();
+  CheckingWater = new CheckingWaterLevelTask(p->getSonar());
+  xTaskCreatePinnedToCore(CheckingWater->tick(),"CheckingWaterLevelTask",10000,NULL,1,&CheckingWaterTask,0);
 }
 
 void loop() {
